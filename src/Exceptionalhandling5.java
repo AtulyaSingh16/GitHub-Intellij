3c6f63c2cd3d0685a09c53512990d25e3e446edd
Atulya Singh <atulyasingh1684@gmail.com>
2025-07-26 07:57:00 +0530

    init commit

--- src/Exceptionalhandling5.java
+++ src/Exceptionalhandling5.java
@@ -1,2 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class Exceptionalhandling5 {
+    public static void main(String[] args) {
+        try
+        {
+            BufferedReader br= new BufferedReader(new FileReader("data.txt"));
+            String line = br.readLine();
+            System.out.println("First line : "+line);
+        }
+        catch (IOException e){
+            System.out.println("Error : "+e.getMessage());
+        }
+    }
 }
